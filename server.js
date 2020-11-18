71cd2e283606c963a742fe53804c55fd579a86df
Mo Uhlig <mouhlig@gmail.com>
2020-11-18 12:06:32 +0100

    Email check added

--- server.js
+++ server.js
@@ -48,11 +48,13 @@ app.post('/newUser', function(req,res)
         const param_loginname = req.body.loginname;
         const param_password1 = req.body.password1;
         const param_password2 = req.body.password2;
-        //hier brauchen wir eventuell noch einen Check ob die eingegebene Email "plausibel" ist.
+        //hier wird gechecked ob die Email "plausibel" ist.
+        const check = param_email.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+    
         db.all(`SELECT * FROM allusers WHERE loginname='${param_loginname}'`,
         function (err, rows)
         {
-            if (rows.length == 0 && param_password1 == param_password2)  // Test ob User bereits existiert und ob Passwort wiederholung korrekt
+            if (rows.length == 0 && param_password1 == param_password2 && param_email.match && check != null)  // Test ob User bereits existiert und ob Passwort wiederholung korrekt
             {
                 const hash = bcrypt.hashSync(param_password1,10);
                 db.run(
