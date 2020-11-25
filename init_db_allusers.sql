587f652865490ab5e76283461a9e2de01131e09f
Mo Uhlig <mouhlig@gmail.com>
2020-11-25 12:20:13 +0100

    user db sql edited

--- init_db_allusers.sql
+++ init_db_allusers.sql
@@ -1,7 +1,11 @@
 /*Tabele erzeugen*/
 CREATE TABLE allusers(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
+    time TEXT NOT NULL,
     email TEXT NOT NULL,
     loginname TEXT NOT NULL,
-    password TEXT NOT NULL
+    password TEXT NOT NULL,
+    role TEXT NOT NULL,
+    favorites TEXT NOT NULL,
+    status INTEGERNOT NULL
 );
